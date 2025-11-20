94444238ba21b07143fd1ba3ba931d674a39edbe
Ayush Srivastava <ayush.srivastava@admiralindia.com>
2025-11-20 22:45:55 +0530

    files structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h2>
+          Hello from <strong>React</strong>
+        </h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
